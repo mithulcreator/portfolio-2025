bfb3f845bd69c013c04e2ecb865ad6dd904b8609
mithulcreator <mithulgaiart@gmail.com>
2025-06-09 11:58:12 +0400

    Fix Project component: Simplify and ensure proper file structure

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Work from './pages/Work';
 import Contact from './pages/Contact';
-import Project from '@/pages/Project';
+import Project from './pages/Project.jsx';
 import AnimatedBlobs from './components/AnimatedBlobs';
 
 function App() {
